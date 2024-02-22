a916c0745775a9f6bb088983209df9d18013f038
Alisa Zhang <86392466+alisa-zhang@users.noreply.github.com>
2024-02-22 10:19:35 -0500

    Added comments to thing.py

--- thing.py
+++ thing.py
@@ -1,3 +1,5 @@
+# takes in the two values through user input
 num1 = float(input("What is your first number?"))
 num2 = float(input("What is your second number?"))
+# prints the product of two numbers
 print(num1*num2)
